0df0675e9fdc567495ca40f2df5bcf8e243d365d
Ankur Oberoi <aoberoi@gmail.com>
2015-09-17 13:39:16 -0700

    Initial Commit

---
+++ ImagePickerAndPublisher/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImagePickerAndPublisher
+//
+//  Created by Ankur Oberoi on 9/17/15.
+//  Copyright © 2015 Ankur Oberoi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImagePickerAndPublisher/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImagePickerAndPublisher
+//
+//  Created by Ankur Oberoi on 9/17/15.
+//  Copyright © 2015 Ankur Oberoi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
